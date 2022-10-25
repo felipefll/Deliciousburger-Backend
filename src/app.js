cb20c54adfaa5458a18dae3898f0f1b5fc328020
felipefll <felipeferreiralopeslima@gmail.com>
2022-10-25 12:48:53 -0300

    Atualização de categoria

--- src/app.js
+++ src/app.js
@@ -13,7 +13,11 @@ class App {
     middleware() {
         this.app.use(express.json())
         this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads'))
-    )}
+    )
+
+    this.app.use('/category-file', express.static(resolve(__dirname, '..', 'uploads'))
+    )
+}
     routes() {
         this.app.use(routes)
     }
