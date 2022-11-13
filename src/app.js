3514cea7aa3fb3e39dd08cbccc8ac1f66c746b8f
felipefll <felipeferreiralopeslima@gmail.com>
2022-11-13 17:54:39 -0300

    atualizações de user

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from 'express'
 import routes from './routes'
 import {resolve} from 'path'
+import cors from 'cors'
+
 import './database'
 
 class App {
     constructor() {
         this.app = express()
+        this.app.use(cors())
+        
         this.middleware()
         this.routes()
-
-    }
+        
+            }
     middleware() {
         this.app.use(express.json())
         this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads'))
