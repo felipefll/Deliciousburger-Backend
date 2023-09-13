15bbc53b35c62f978fe47742473c9294f89a9028
felipefll <felipeferreiralopeslima@gmail.com>
2023-09-13 16:17:25 -0300

    correction database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: "postgres",
   url: "postgresql://postgres:7fxRPfRwWAHPzyFMDvDC@containers-us-west-103.railway.app:7640/railway",
   define: {
-    timespamps: true,
+    timestamps: true,
     underscored: true,
     underscoredALL: true,
   },
