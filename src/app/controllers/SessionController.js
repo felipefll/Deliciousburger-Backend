f2299e46a4b339d78c1557f4574e80b13a1e3f46
felipefll <felipeferreiralopeslima@gmail.com>
2023-09-14 15:40:22 -0300

    fix session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,45 +1,41 @@
-import * as Yup from 'yup'
-import jwt from 'jsonwebtoken'
-import authconfig from '../../config/auth'
-import User from '../models/User'
-
+import * as Yup from "yup";
+import jwt from "jsonwebtoken";
+import authconfig from "../../config/auth";
+import User from "../models/User";
 
 class SessionController {
-    async store(request, response) {
-        const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().required(),
-        })
-        const userEmailOrPasswordIncorrect = () => {
-            return response
-            .status(401)
-            .json({ error: 'Make sure your password or email are correct' })
-        }
-        if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect()
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+    const userEmailOrPasswordIncorrect = () => {
+      return response
+        .status(401)
+        .json({ error: "Make sure your password or email are correct" });
+    };
+    if (!(await schema.isValid(request.body))) userEmailOrPasswordIncorrect();
 
-        const { email, password } = request.body
-        
-        const user = await User.findOne({
-            where: { email },
-        })
+    const { email, password } = request.body;
 
-        if (!user) userEmailOrPasswordIncorrect()
+    const user = await User.findOne({
+      where: { email },
+    });
 
+    if (!user) userEmailOrPasswordIncorrect();
 
-        if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
+    if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect();
 
-
-        return response.json({
-            id: user.id,
-            email,
-            name: user.name,
-            admin: user.admin,
-            token: jwt.sign({ id: user.id, name: user.name }, authconfig.secret, {
-                expiresIn: authconfig.expiresIn,
-                
-            }),
-        })
-    }
+    return response.json({
+      id: user.id,
+      email,
+      name: user.name,
+      admin: user.admin,
+      token: jwt.sign({ id: user.id, name: user.name }, authconfig.secret, {
+        expiresIn: authconfig.expiresIn,
+      }),
+    });
+  }
 }
 
-export default new SessionController()
+export default new SessionController();
